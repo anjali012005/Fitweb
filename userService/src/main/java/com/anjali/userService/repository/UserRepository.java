3656b5cae5bec0e5370ad3160558f8b6b014bea7
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-12-19 18:51:52 +0530

    Integrated keyCloack in userService

--- userService/src/main/java/com/anjali/userService/repository/UserRepository.java
+++ userService/src/main/java/com/anjali/userService/repository/UserRepository.java
@@ -3,10 +3,15 @@ package com.anjali.userService.repository;
 import com.anjali.userService.model.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import org.jspecify.annotations.Nullable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByEmail(@NotBlank(message = "Email is required") @Email(message = "Invalid email format") String email);
+
+    @Nullable Boolean existsByKeycloakId(String userId);
+
+    User findByEmail(@NotBlank(message = "Email is required") @Email(message = "Invalid email format") String email);
 }
